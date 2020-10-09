ea26e8514f688f60c012ec9ec917aa03e9ac3e26
Jahara Clark <jaharalewis@hotmail.com>
2020-10-08 17:03:38 -1000

    Completed meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
+function nameMenuItem(firstName) {
+  return `Delicious ${firstName}`;
+};
+nameMenuItem("Pizza");
 
-}
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+  name: name,
+  price: price,
+  type: type,
+  };
+  return menuItem
+};
+
+function addIngredients(customerRequest, ingredients){
+  if (ingredients.includes(customerRequest)) {
+    return ingredients
+  } else {
+  return(ingredients.push(customerRequest));
+};
+};
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+};
+
+function decreasePrice(price) {
+  return price * .9
+};
+
+function createRecipe(title, ingredients, type) {
+    var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  };
+    return recipe
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
