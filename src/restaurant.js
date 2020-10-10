9c96649d1aa4a8f80f4c5c203d6cfad30d290896
Jahara Clark <jaharalewis@hotmail.com>
2020-10-10 13:41:20 -1000

    Completed restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,35 @@
+function createRestaurant(restaurantName) {
+  return {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  };
+};
+
+
+function addMenuItem (nameOfRestaurant, choice) {
+  if (nameOfRestaurant.menus[choice.type].includes(choice) === false ) {
+    nameOfRestaurant.menus[choice.type].push(choice)
+  }
+};
+
+function  removeMenuItem(nameOfRestaurant, lostItem, mealType){
+  var searchForItem = nameOfRestaurant.menus[mealType].indexOf(lostItem);
+  for (var i = 0; i < nameOfRestaurant.menus[mealType].length; i++) {
+    if (nameOfRestaurant.menus[mealType][i].name === lostItem) {
+      nameOfRestaurant.menus[mealType].splice(searchForItem, 1);
+    }
+  }
+  return `Sorry, we don't sell ${lostItem}, try adding a new recipe!`
+};
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
